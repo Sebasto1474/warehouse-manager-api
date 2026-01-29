992d10e2013087008cff01ab95cf25b5f29864f1
Sebasto1474 <sebastiang.ledesma@gmail.com>
2026-01-29 11:53:10 +0000

    add update materials

--- app/domain/entities/material.py
+++ app/domain/entities/material.py
@@ -6,16 +6,3 @@ class Material:
 
     def __str__(self):
         return f"Material code : {self.material_id} - {self.description}"
-
-    def increase_quantity(self, value):
-        if value <= 0:
-            raise ValueError("Value must be greater than 0.")
-        self.quantity += value
-
-    def decrease_quantity(self, value):
-        if value <= 0:
-            raise ValueError("Value must be greater than 0.")
-        if value > self.quantity:
-            raise ValueError("Value cannot be greater than current stock.")
-        self.quantity -= value
-
