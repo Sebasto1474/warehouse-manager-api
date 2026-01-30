f7eb2ba0bc87e3890669911e94035cf6f269fbdb
Sebasto1474 <sebastiang.ledesma@gmail.com>
2026-01-30 12:40:16 +0000

    add transfer_service --> transfer_out

--- app/domain/services/transfer_service.py
+++ app/domain/services/transfer_service.py
@@ -9,7 +9,7 @@ class TransferServices:
         if quantity <= 0:
             raise ValueError("Quantity must be positive.")
         if not destination_location.available():
-            raise TypeError("Location must be available")
+            raise ValueError("Location must be available")
         if not self.material_repo.get_by_id(material_id):
             new_material = self.material_repo.create(material_id)
         stock_in = self.stock_repo.get_stock(destination_location, material_id)
@@ -19,5 +19,20 @@ class TransferServices:
         new_transfer = Transfer(user,trf_type,destination_location, quantity )
         self.transfer_repo.save(new_transfer)
         return new_transfer
-        
-        
+
+
+    def transfer_out(self, user, origin_location, material_id, quantity):
+        trf_type = "Out"
+        if quantity <= 0:
+            raise ValueError("Quantity must be positive.")
+        if origin_location.available():
+            raise ValueError("Location must be occupied.")
+        if not self.material_repo.get_by_id(material_id):
+            raise ValueError("The material ID does not exist.")
+        stock_out = self.stock_repo.get_stock(origin_location, material_id)
+        if not stock_out:
+            raise ValueError("Stock does not exist.")
+        stock_out.decrease_stock(quantity)
+        new_transfer = Transfer(user, trf_type, origin_location, quantity)
+        self.transfer_repo.save(new_transfer)
+        return new_transfer
