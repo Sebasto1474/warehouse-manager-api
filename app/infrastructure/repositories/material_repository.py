9b9ebf96bd11c420f2cd1ced1ff0af68cb2a5f82
Sebasto1474 <sebastiang.ledesma@gmail.com>
2026-02-02 14:45:33 +0000

    work in:  material_repository

--- app/infrastructure/repositories/material_repository.py
+++ app/infrastructure/repositories/material_repository.py
@@ -1,11 +1,16 @@
 class MaterialRepository:
     def __init__(self):
-        pass
+        self.materials_id = {}
 
     def get_by_id(self, material_id):
-        pass
+        if material_id in self.materials_id:
+            material_tgt = self.materials_id[material_id]
+        else:
+            return None
+        return material_tgt
 
     def create(self, material_id, description):
         new_material = Material(material_id, description)
+        self.materials_id[material_id] = new_material # saving material id 
         return new_material
 
